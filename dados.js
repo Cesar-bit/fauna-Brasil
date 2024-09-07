f162102a9eac26d4e08a193b1806c61a5f98ca75
Silvio Cesar Dias <67872041+Cesar-bit@users.noreply.github.com>
2024-09-07 19:09:30 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,62 @@
+let dados = [
+    {
+        titulo: "Boto cor de rosa",
+        descricao: "O boto-cor-de-rosa é um mamífero que vive na água. Ele é parente das baleias e dos golfinhos: a diferença é que ele não vive no mar, mas sim na água doce. Seu habitat são os rios amazônicos do Brasil, da Bolívia, da Venezuela, da Colômbia, do Equador e do Peru.",
+        link: "https://pt.wikipedia.org/wiki/Boto-cor-de-rosa",
+        tags: "golfo golfinhos golfinho Boto boto rosa"
+    },
+    {
+        titulo: "Mico leão Dourado",
+        descricao: "O mico-leão-dourado é um dos animais típicos do Brasil mais famosos. Ele habita apenas na Mata Atlântica brasileira, nos estados do Rio de Janeiro e Espírito Santo.",
+        link: "https://pt.wikipedia.org/wiki/Mico-leão-dourado",
+        tags: "Mico macaco dourado"
+    },
+    {
+        titulo: "Arara azul",
+        descricao: "A arara-azul é uma espécie de ave da mesma família dos papagaios e periquitos. O termo azul refere-se à coloração marcante dessa espécie encontrada na Bolívia, Paraguai e no nosso país, onde é possível observá-la na Floresta Amazônica, no Cerrado e no Pantanal.",
+        link: "https://pt.wikipedia.org/wiki/Arara-azul-grande",
+        tags: "Arara arara azul"
+    },
+	{
+        titulo: "Jaguatirica",
+        descricao: "A jaguatirica (nome científico: Leopardus pardalis), também conhecida como ocelote, é uma espécie de mamífero carnívoro pertencente à família dos felídeos, sendo um dos dez representantes do gênero Leopardus. São reconhecidas dez subespécies, com o gato-maracajá (L. wiedii) sendo a espécie mais próxima da jaguatirica.",
+        link: "https://pt.wikipedia.org/wiki/Jaguatirica",
+        tags: "Jaguatirica jaguatirica ocelote gato do mato"
+    },
+	{
+	 titulo: "Sucuri",
+        descricao: "Também conhecida como anaconda-verde, essa espécie de cobra vive na Amazônia, nas regiões alagadas, cavernas e na floresta sempre perto das margens dos rios.",
+        link: "https://pt.wikipedia.org/wiki/Sucuri-verde",
+        tags: "Cobra cobra Sucuri sucuri anaconda-verde"
+	},
+	{
+	 titulo: "Capivara",
+        descricao: "As capivaras são a maior espécie de roedores do mundo. Esses animais típicos do Brasil podem ser encontradas também na América Central e em outros países da América do Sul, em geral nas regiões próximas a rios e lagos.",
+        link: "https://pt.wikipedia.org/wiki/Capivara",
+        tags: "Capivara capivara roedor roedores"
+	},
+	{
+	 titulo: "Ariranha",
+        descricao: "A ariranha, também conhecida como lontra-gigante, é um mamífero carnívoro que pertence à mesma família das lontras. Ela vive em quase toda a América do Sul, sendo encontrada em países como Brasil, Bolívia, Peru, Paraguai, Venezuela e Equador.",
+        link: "https://pt.wikipedia.org/Mustelídeos",
+        tags: "Ariranha ariranha lontra Mustelídeos"
+	},
+	{
+	 titulo: "Lobo guará",
+        descricao: "O lobo-guará (nome científico: Chrysocyon brachyurus), também chamado guará, aguará, aguaraçu, lobo-de-crina, lobo-de-juba e lobo-vermelho, é uma espécie de canídeo endêmico da América do Sul. Suas marcas lembram as de uma raposa, mas não é uma raposa nem um lobo. É a única espécie do gênero Chrysocyon e provavelmente, a espécie vivente mais próxima é o cachorro-vinagre (Speothos venaticus).",
+        link: "https://pt.wikipedia.org/wiki/Lobo-guará",
+        tags: "Lobo lobo guará guara "
+	},
+	{
+	 titulo: "Tucano toco",
+        descricao: "Ramphastos toco, popularmente conhecido como tucanuçu[2], tucanaçu, tucano-grande e tucano-boi,[3] é uma espécie de tucano e o maior representante da família Ramphastidae.",
+        link: "https://pt.wikipedia.org/wiki/Tucano-toco",
+        tags: "tucano Tucano Tucano-toco tucanuçu"
+	},
+	{
+	 titulo: "Papagaio",
+        descricao: "O papagaio-verdadeiro é principalmente um papagaio verde com cerca de 37centímetros de comprimento[6] e pesa cerca de 400 gramas. Tem penas azuis na testa, acima do bico e amarelo na cara e coroa. Distribuição do azul e amarelo varia muito. A cor da íris dos adultos é amarelo-laranja no macho ou vermelho-laranja na fêmea.",
+        link: "https://pt.wikipedia.org/wiki/Papagaio-verdadeiro",
+        tags: "Papagaio-verdadeiro Papagaio papagaio"
+	}
+];
